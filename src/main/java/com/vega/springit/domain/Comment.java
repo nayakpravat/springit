933aacdf6254e5d45c55be680871eedb96041384
nayakpravat <nayak.pravat05@example.com>
2021-07-13 14:35:32 -0400

    Added the model files

---
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -0,0 +1,23 @@
+package com.vega.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment extends Auditable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+
+    //Link
+    @ManyToMany
+    private Link link;
+}
---
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -0,0 +1,26 @@
+package com.vega.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Link extends Auditable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String url;
+
+    //comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<Comment>();
+}
